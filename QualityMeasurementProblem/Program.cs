70ca6eac760f4184aa69badf7eb318e3d45f5fbc
nitikeshs58 <nitikeshs58@gmail.com>
2020-05-07 11:24:41 +0530

    Added: Welcome message to Quality Measurement Problem.

---
+++ QualityMeasurementProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QualityMeasurementProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Quality Measurement Problem!");
+        }
+    }
+}
